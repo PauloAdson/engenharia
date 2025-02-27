092f14507ceea53be6b879abceeec2b786a0c7de
Paulo Adson <paulo.alves200581@gmail.com>
2025-02-27 12:38:17 -0300

    adicionando arquivos

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,19 @@ function atualizarConteudo(numero, titulo, texto, elemento = null) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    atualizarConteudo('01', 'Ecoconstrua', '1');
+    atualizarConteudo('01', 'Ecoconstrua', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem magnam eveniet, sequi, fugiat veniam beatae dolorum quidem repudiandae modi sit, commodi hic soluta id illo nobis deserunt eius dicta dolorem.');
 
     document.querySelector('.conteudo-tabela-projetos').classList.add('selecionado');
 });
+
+const slider = document.querySelector('.clientes_slider');
+const PrevButton = document.querySelector('.prev');
+const NextButton = document.querySelector('.next');
+
+PrevButton.addEventListener('click', () => {
+    console.log('estou aqui prev');
+})
+
+NextButton.addEventListener('click', () => {
+    console.log('estou aqui next');
+})
